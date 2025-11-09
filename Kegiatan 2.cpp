78fc1576672209504d82e5ebaf25304eed7501c4
haikalpratama-ALT <haikalpratama@mail.ugm.ac.id>
2025-11-09 22:52:03 +0700

    Update Kegiatan 2.cpp

--- Kegiatan 2.cpp
+++ Kegiatan 2.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 //fungsi baca data
-ifstream inputFile("input_data.txt");
 
 int maksimumRekursif(int arr[], int n) {
 if (n == 1) return arr[0];
@@ -12,10 +11,22 @@ return (arr[n - 1] > maxSubarray) ? arr[n - 1] : maxSubarray;
 }
 
 int main() {
-int arr[] = <inputFile>
-cin << inputFile;
-int n = sizeof(arr) / sizeof(arr[0]);
-arr[] >> inputFile;
-cout << "Nilai maksimum dalam array: " << maksimumRekursif(arr, n) << endl;
-return 0;
+    ifstream input("input_data.txt"); 
+    if (!input) {
+        cout << "File tidak dapat dibuka!" << endl;
+        return 1;
+    }
+    int arr[100];
+    int n = 0;
+    while (input >> arr[n]) {
+        n++;
+    }
+    input.close(); 
+    cout << "Data yang dibaca: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << "Nilai maksimum dalam array: " << maksimumRekursif(arr, n) << endl;
+    return 0;
 }
